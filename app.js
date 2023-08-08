6fcb2a4a292697cde95efccf53559e6c2adf8b58
DevAzizbek <azizbekakramjonov0910@gmail.com>
2023-08-08 19:42:33 +0500

    infinizai

---
+++ app.js
@@ -0,0 +1,9 @@
+// LOADER
+
+// var loader = document.querySelector(".loader")
+
+// window.addEventListener("load", vanish);
+
+// function vanish(){
+//     loader.classList.add("disppear");
+// }
